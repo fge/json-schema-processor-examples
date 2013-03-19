eadc61df6c70b12f8f531f561409703cffbc1d92
Francis Galiegue <fgaliegue@gmail.com>
2013-03-19 21:55:31 +0100

    Add json-schema-avro dependency
    Remove all tests accordingly
    
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- src/main/java/com/github/fge/jsonschema2avro/UnsupportedJsonSchemaException.java
+++
@@ -1,8 +0,0 @@
-package com.github.fge.jsonschema2avro;
-
-import com.github.fge.jsonschema.exceptions.ProcessingException;
-
-public final class UnsupportedJsonSchemaException
-    extends ProcessingException
-{
-}
