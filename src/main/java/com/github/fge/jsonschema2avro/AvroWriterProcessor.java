28c6a0ef0093c2f56d18186a3eb0670dd93ab678
Francis Galiegue <fgaliegue@gmail.com>
2013-03-19 13:21:11 +0100

    AvroWriterProcessor: remove main()
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- src/main/java/com/github/fge/jsonschema2avro/AvroWriterProcessor.java
+++ src/main/java/com/github/fge/jsonschema2avro/AvroWriterProcessor.java
@@ -1,15 +1,10 @@
 package com.github.fge.jsonschema2avro;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.github.fge.jsonschema.exceptions.ProcessingException;
 import com.github.fge.jsonschema.processing.Processor;
 import com.github.fge.jsonschema.processing.ProcessorSelector;
 import com.github.fge.jsonschema.processors.data.SchemaHolder;
-import com.github.fge.jsonschema.report.ConsoleProcessingReport;
 import com.github.fge.jsonschema.report.ProcessingReport;
-import com.github.fge.jsonschema.tree.CanonicalSchemaTree;
-import com.github.fge.jsonschema.tree.SchemaTree;
-import com.github.fge.jsonschema.util.JsonLoader;
 import com.github.fge.jsonschema.util.ValueHolder;
 import com.github.fge.jsonschema2avro.writers.ArrayWriter;
 import com.github.fge.jsonschema2avro.writers.EnumWriter;
@@ -19,8 +14,6 @@ import com.github.fge.jsonschema2avro.writers.SimpleTypeWriter;
 import com.github.fge.jsonschema2avro.writers.SimpleUnionWriter;
 import com.github.fge.jsonschema2avro.writers.TypeUnionWriter;
 import org.apache.avro.Schema;
-
-import java.io.IOException;
 
 import static com.github.fge.jsonschema2avro.predicates.AvroPredicates.*;
 
@@ -49,19 +42,5 @@ public final class AvroWriterProcessor
     {
         final AvroPayload payload = new AvroPayload(input.getValue(), this);
         return processor.process(report, payload);
-    }
-
-    public static void main(final String... args)
-        throws ProcessingException, IOException
-    {
-        final JsonNode node
-            = JsonLoader.fromString("{\"type\": \"array\"," +
-            "\"additionalItems\":{\"type\":\"null\"}}");
-        final SchemaTree tree = new CanonicalSchemaTree(node);
-        final SchemaHolder input = new SchemaHolder(tree);
-        final AvroWriterProcessor p = new AvroWriterProcessor();
-        final ProcessingReport report = new ConsoleProcessingReport();
-
-        System.out.println(p.process(report, input).getValue());
     }
 }
